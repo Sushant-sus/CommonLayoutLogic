cd3a4ab19d483d57237a4aed97f40f70461bdf34
Seaton <sushant902.sus@gmail.com>
2024-02-09 11:26:24 +0545

    2nd

--- src/components/layout/CommonLayout.jsx
+++ src/components/layout/CommonLayout.jsx
@@ -5,14 +5,14 @@ import { Outlet } from "react-router-dom";
 
 const CommonLayout = () => {
   return (
-    <div className="h-screen w-full md:w-full grid grid-rows-12 grid-cols-12 text-xs">
-      <header className="col-span-12 w-full h-[52px] border-b-[1px] border-slate-1000 px-4 flex justify-between items-center bg-grey-200 gap-2">
+    <div className="h-screen w-screen md:w-full grid grid-rows-12 grid-cols-12 text-xs">
+      <header className="col-span-12 w-full h-[65px] border-b-[1px] border-slate-1000 px-4 flex justify-between items-center bg-grey-200 gap-2">
         <span className="text-lg font-bold">Header Area</span>(Layouts - Header
         and Sidebar - Implement in auth - module using module federation)
       </header>
-      <div className="flex h-[calc(100vh-52px)] w-screen">
+      <div className="flex h-[calc(100vh-65px)] w-screen">
         <Sidebar />
-        <main className="flex-1 max-w-full max-h-screen overflow-y-scroll">
+        <main className="flex h-full w-full bg-red-200 ">
           <Outlet />
         </main>
       </div>
